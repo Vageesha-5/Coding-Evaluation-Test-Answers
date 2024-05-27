ae6c5b4b1592dfd266487b4a98215d3a31307f49
Vageesha Theertha C <121344137+Vageesha-5@users.noreply.github.com>
2024-05-27 09:32:52 +0530

    Delete (2)second.js

--- (2)second.js
+++
@@ -1,9 +0,0 @@
-//2.	Add a method called introduce to the person object. The introduce method should take one parameter, age, and log the message "Hello, my name is [name] and I am [age] years old" to the console.
-
-const person = {
-    name: "Raj",
-    introduce: function(age) {
-        console.log(`Hello, my name is ${this.name} and I am ${age} years old`);
-    }
-};
-person.introduce(25); // Output: Hello, my name is Raj and I am 25 years old
